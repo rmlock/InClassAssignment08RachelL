e7a1d552ae1b5bb9a78de5928e9126610280e197
Rachel Lockerman <rl2856@tc.columbia.edu>
2017-03-30 19:33:14 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/android/inclassassignment08/MainActivity.java
@@ -0,0 +1,150 @@
+package com.example.android.inclassassignment08;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private String TAG = "MainActivity";
+    FirebaseDatabase database;
+    String name;
+    EditText named;
+    String displayName;
+    DatabaseReference myRef;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+
+//                    FirebaseUser userL = FirebaseAuth.getInstance().getCurrentUser();
+                    String uid = user.getUid();
+
+                    // Name, email address, and profile photo Url
+                    name = user.getDisplayName();
+                    String email = user.getEmail();
+//                        Uri photoUrl = user.getPhotoUrl();
+                    named = (EditText) findViewById(R.id.name);
+                    displayName = named.getText().toString();
+
+                    myRef = database.getReference(uid).child("UserInfo");
+
+                    // Read from the database
+                    myRef.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            // This method is called once with the initial value and again
+                            // whenever data at this location is updated.
+                            String value = dataSnapshot.getValue(String.class);
+                            Log.d(TAG, "Value is: " + value);
+                            TextView textView = (TextView) findViewById(R.id.CurrentName);
+                            textView.setText(value);
+
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError error) {
+                            // Failed to read value
+                            Log.w(TAG, "Failed to read value.", error.toException());
+                        }
+                    });
+
+
+                    // Check if user's email is verified
+                    boolean emailVerified = user.isEmailVerified();
+
+                    // The user's ID, unique to the Firebase project. Do NOT use this value to
+                    // authenticate with your backend server, if you have one. Use
+                    // FirebaseUser.getToken() instead.
+
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    Intent intent = new Intent(MainActivity.this, UserInfo.class);
+                    startActivity(intent);
+                }
+                // ...
+            }
+        };
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return true;
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.logOut:
+                mAuth.signOut();
+                return true;
+
+            case R.id.about:
+                Toast.makeText(this, "This app is all about fire. This app is on FIIREEEEEE", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+
+    public void updateName(View view) {
+        EditText updatedNameEditText = (EditText) findViewById(R.id.name);
+        String newName = updatedNameEditText.getText().toString();
+        updatedNameEditText.setText(newName);
+        myRef.setValue(newName);
+        TextView shownName = (TextView) findViewById(R.id.CurrentName);
+        shownName.setText(newName);
+        updatedNameEditText.setText("");
+
+
+    }
+}
